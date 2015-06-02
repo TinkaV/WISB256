ea3f52398c91656b48c721055e8dc3d7e80938d6
tinkav <t.veldhuis@students.uu.nl>
2015-06-02 09:32:31 +0000

    Opdracht 5 af

--- Opdracht5/Vector.py
+++ Opdracht5/Vector.py
@@ -1,18 +1,39 @@
+from array import array
+
 class Vector(object):
-    def __init__(self,n,other):
-        self.len = n
+    def __init__(self,n,other=0):
         if isinstance(other, float):
-            self.vector = n*[other]
-        elif isinstance(other, list):
-            self.vector = [float(i) for i in other]
+            self.vector = array('d',n*[other])
         else:
-            self.vector = n*[0.0]
-            
+            self.vector = array('d',other)
+        self.n = n
         
     def __str__(self):
         opscherm = ''
-        for i in range(self.len):
+        for i in range(self.n):
             opscherm += str(self.vector[i]) + '\n'
         return(opscherm)
 
+    def lincomb(self,other,alpha,beta):
+        c = array('d',[])
+        for i in range(self.n):
+            a = alpha*self.vector[i]
+            b = beta*other.vector[i]
+            c.append(a+b)
+        return(Vector(self.n,c))
+
+    def scalar(self,alpha):
+        d = self.lincomb(self,alpha,0)
+        return(d)
+    
+    def inner(self,other):
+        x = 0
+        for i in range(self.n):
+            x += self.vector[i]*other.vector[i]
+        return(x)
+    
+    def norm(self):
+        f = self.inner(self)
+        return(f**0.5)
+        
 
